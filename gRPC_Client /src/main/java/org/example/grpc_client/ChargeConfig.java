7d6232da4cdd85aac13af0780a8bbd94b07b558d
Sumin Choi <rev3kr@gmail.com>
2023-12-11 01:30:19 +0900

    gRPC HW

---
+++ gRPC_Client /src/main/java/org/example/grpc_client/ChargeConfig.java
@@ -0,0 +1,15 @@
+package org.example.grpc_client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+@Configuration
+public class ChargeConfig {
+    @Bean
+    public ManagedChannel getManagedChannel() {
+        return ManagedChannelBuilder.forTarget("127.0.0.1:8080")
+                .usePlaintext()
+                .build();
+    }
+}
